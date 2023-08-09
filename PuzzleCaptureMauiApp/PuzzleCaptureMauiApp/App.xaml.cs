b78dbb923e72891447ff23ab6bed276f008c8749
Kyungwon Jeon <jkwchunjae@daum.net>
2023-08-09 22:25:18 +0900

    Init PuzzleCaptureMauiApp

---
+++ PuzzleCaptureMauiApp/PuzzleCaptureMauiApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PuzzleCaptureMauiApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ PuzzleCaptureMauiApp/PuzzleCaptureMauiApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PuzzleCaptureMauiApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
