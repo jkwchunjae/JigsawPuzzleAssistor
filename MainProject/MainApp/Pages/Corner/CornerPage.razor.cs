ceb5d43f59b66c78e6969fab269bfbcbf5ce7273
Kyungwon Jeon <jkwchunjae@daum.net>
2024-01-07 11:18:53 +0900

    코너 추출

---
+++ MainProject/MainApp/Pages/Corner/CornerPage.razor.cs
@@ -0,0 +1,53 @@
+﻿using MainApp.Service;
+using Microsoft.AspNetCore.Components;
+using PictureToData;
+
+namespace MainApp.Pages.Corner;
+
+public partial class CornerPage : ComponentBase
+{
+    [Inject] NavigationManager NavigationManager { get; set; } = null!;
+    [Inject] WorkspaceService WorkspaceService { get; set; } = null!;
+
+    WorkspaceData? workspace => WorkspaceService.CurrentWorkspace;
+
+    int CornerProgress = 0;
+
+    protected override void OnInitialized()
+    {
+        if (workspace == null)
+        {
+            NavigationManager.NavigateTo("/start");
+        }
+        if (!WorkspaceService.HasCroppedImage())
+        {
+            NavigationManager.NavigateTo("/outline");
+        }
+    }
+
+    Task StartCorner(int thickness)
+    {
+        CornerProgress = 0;
+
+        var cornerService = new ImageCornerService(workspace!);
+        cornerService.CornerProgress += (sender, e) =>
+        {
+            InvokeAsync(() =>
+            {
+                CornerProgress = (int)(e.Processed / (double)e.Total * 100);
+                StateHasChanged();
+            });
+        };
+
+        var cornerDetectArgument = new CornerDetectArgument
+        {
+            MaxCorners = 4,
+            QualityLevel = 0.01,
+            MinDistance = 200,
+            BlockSize = 9,
+        };
+
+        _ = cornerService.StartCorner(cornerDetectArgument, thickness);
+        return Task.CompletedTask;
+    }
+}
---
+++ MainProject/MainApp/Service/ImageCornerService.cs
@@ -0,0 +1,49 @@
+﻿using PictureToData;
+using System.Diagnostics;
+using System.Drawing;
+
+namespace MainApp.Service;
+
+public class ImageCornerService
+{
+    private WorkspaceData workspace;
+    public event EventHandler<ProgressEventArgs>? CornerProgress;
+    public ImageCornerService(WorkspaceData workspace)
+    {
+        this.workspace = workspace;
+    }
+
+    public async Task StartCorner(CornerDetectArgument cornerArgs, int thickness = 1, bool openFolder = false)
+    {
+        if (!Directory.Exists(workspace.CornerDir))
+        {
+            Directory.CreateDirectory(workspace.CornerDir);
+        }
+
+        var pieceService = new PieceService();
+
+        var processed = 0;
+        var files = Directory.GetFiles(workspace.ResizeDir);
+        await Parallel.ForEachAsync(files, async (file, ct) =>
+        {
+            var fileName = Path.GetFileName(file);
+            var outputPath = Path.Join(workspace.CornerDir, fileName);
+            var corners = await pieceService.GetCornerWithArgument(file, cornerArgs);
+            await pieceService.MakeCornerAssistImageAsync(file, outputPath, corners, _ =>
+            {
+                return (Radius: 5, color: Color.White, thickness);
+            });
+            Interlocked.Increment(ref processed);
+            CornerProgress?.Invoke(this, new ProgressEventArgs
+            {
+                Total = files.Length,
+                Processed = processed,
+            });
+        });
+
+        if (openFolder)
+        {
+            Process.Start("explorer.exe", workspace.CornerDir);
+        }
+    }
+}
