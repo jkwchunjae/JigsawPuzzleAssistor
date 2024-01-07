bbfd4f0144d6eb82f6d8ca15ec973a897b9c3408
Kyungwon Jeon <jkwchunjae@daum.net>
2024-01-07 23:22:16 +0900

    모서리 연결 정보

---
+++ MainProject/MainApp/Pages/SortedEdge/SortedEdgePage.razor.cs
@@ -0,0 +1,52 @@
+﻿using Common.PieceInfo;
+using JkwExtensions;
+using MainApp.Service;
+using Microsoft.AspNetCore.Components;
+using System.Text.Json;
+
+namespace MainApp.Pages.SortedEdge;
+
+public record EdgeInfo(string Piece1, string Piece2, float Value);
+
+public partial class SortedEdgePage : ComponentBase
+{
+    [Inject] NavigationManager NavigationManager { get; set; } = null!;
+    [Inject] WorkspaceService WorkspaceService { get; set; } = null!;
+
+    WorkspaceData? workspace => WorkspaceService.CurrentWorkspace;
+
+    ConnectInfo[] connections = Array.Empty<ConnectInfo>();
+    EdgeInfo[] SortedEdges = Array.Empty<EdgeInfo>();
+
+    protected override async Task OnInitializedAsync()
+    {
+        if (workspace == null)
+        {
+            NavigationManager.NavigateTo("/start");
+            return;
+        }
+        else if (!WorkspaceService.HasCroppedImage())
+        {
+            NavigationManager.NavigateTo("/outline");
+            return;
+        }
+
+        var files = Directory.GetFiles(workspace!.ConnectionDir);
+        var connectionInfos = await files
+            .Select(x => ReadFromFile(x))
+            .WhenAll();
+
+        SortedEdges = connectionInfos
+            .SelectMany(x => x.Edges.SelectMany(y => y.Connection.Select(z => new EdgeInfo(x.PieceName, z.PieceName, Value: z.Value))))
+            .Select(x => x.Piece1.CompareTo(x.Piece2) < 0 ? x : new EdgeInfo(x.Piece2, x.Piece1, x.Value))
+            .Distinct()
+            .OrderBy(x => x.Value)
+            .ToArray();
+    }
+
+    private static async Task<ConnectInfo> ReadFromFile(string path)
+    {
+        var text = await File.ReadAllTextAsync(path);
+        return JsonSerializer.Deserialize<ConnectInfo>(text)!;
+    }
+}
